0de5335c059328436c80ca3c63d397bc0a493fec
mufida1018 <mufidapatel03@gmail.com>
2022-05-08 16:47:22 +0530

    Dicee

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main(){
+  runApp(Dicee());
+}
+class Dicee extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+
